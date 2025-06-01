3c8b365076cb38cc295afa2b10c13af4022f52bf
Lukas Potthast <privat@lukas-potthast.de>
2025-06-01 14:21:58 +0200

    Fix type visibility; Include `port` types in prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 mod cache;
-mod chromedriver;
+pub mod chromedriver;
 mod download;
-mod mgr;
-mod port;
-mod session;
+pub mod mgr;
+pub mod port;
+pub mod session;
 
 pub mod prelude {
     pub use crate::chromedriver::Chromedriver;
     pub use crate::mgr::ChromeForTestingManager;
     pub use crate::mgr::VersionRequest;
+    pub use crate::port::Port;
+    pub use crate::port::PortRequest;
     pub use crate::session::Session;
     pub use chrome_for_testing::api::channel::Channel;
     pub use chrome_for_testing::api::version::Version;
